2966984a488db9566a151420aee9e41cfae7db71
Ninaad A K <126323997+ninaadak03@users.noreply.github.com>
2024-09-20 23:32:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 const summarizeText = require('./summarize.js');
 
 // Parses JSON bodies (as sent by API clients)
